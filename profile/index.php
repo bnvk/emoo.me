4936b5460a1dedd6bc1d51972c810c1976f5b5e2
Brennan Novak <brennan@reverseproductions.com>
2011-10-01 14:48:23 -0700

    initial git commit

---
+++ profile/index.php
@@ -0,0 +1,25 @@
+<div id="profile_image">
+	<img src="<?= $this->social_igniter->profile_image($user_id, $image, $gravatar, 'large'); ?>" border="0">
+</div>
+
+<div id="profile_info">
+	<ul>
+		<li><h1><?= $name ?></h1></li>
+		<li><h3>@<?= $username ?></h3></li>
+		<?= display_value("li", "", "", $company); ?>
+		<?= display_value("li", "", "", $location); ?>
+		<?= display_value("li", "", "", display_link("", "", $url, $url)); ?>		
+		<?= display_value("li", "", "", $bio); ?>
+	</ul>	
+</div>
+<div class="clear"></div>
+<div class="norm_separator"></div>
+
+<ol id="profile_feed">
+	<?= $timeline_view ?>
+</ol>
+<div class="clear"></div>
+
+<?php if ($timeline_count > 8): ?>
+<input type="button" id="profile_feed_see_more" value="See More">
+<?php endif; ?>
