b1c66213daf27f633014b0c438e7c2671479ce09
Brennan Novak <brennan@reverseproductions.com>
2012-04-09 09:33:50 -0700

    added qtip, made visualizatio purty

--- partials/logged.php
+++ partials/logged.php
@@ -1,6 +1,5 @@
 <div id="header">
 	<a id="header_home" href="<?= base_url() ?>"><img src="<?= $site_assets ?>apple-touch-icon-72x72-precomposed.png"><img id="header_name" src="<?= $site_assets ?>emoome-name.png"></a>
-
 	<ul id="header_links">	
 	<?php if ($this->social_auth->logged_in()): ?>
 		<li><a class="button" href="<?= base_url() ?>emoome/contribute">Contribute</a></li>	
