7578976ce916d56a833b60b8c953c934e939d0f6
Brennan Novak <brennan@reverseproductions.com>
2012-04-12 19:09:01 -0700

    fixed some minor things

--- partials/logged.php
+++ partials/logged.php
@@ -1,13 +1,8 @@
 <div id="header">
-	<a id="header_home" href="<?= base_url() ?>">
-		<img src="<?= $site_assets ?>apple-touch-icon-72x72-precomposed.png">
-		<?php if ($this->uri->segment(1)): ?>
-		<img id="header_name" src="<?= $site_assets ?>emoome-name.png">
-		<?php endif; ?>
-	</a>
+	<a id="header_home" href="<?= base_url() ?>"><img src="<?= $site_assets ?>apple-touch-icon-72x72-precomposed.png"></a>
 	<ul id="header_links">	
 	<?php if ($this->social_auth->logged_in()): ?>
-		<li><a class="button" href="<?= base_url() ?>record">Record</a></li>	
+		<li><a class="button" href="<?= base_url() ?>record/feeling">Record</a></li>	
 		<li><a class="button" href="<?= base_url() ?>visualize">Visualize</a></li>	
 		<li><a class="button" href="<?= base_url() ?>emoome/user">Settings</a></li>	
 		<li><a class="button" href="<?= $link_logout ?>">Log Out</a></li>
