39c7ea17d478f4aac2ffa92328317d1059abdd0f
Brennan Novak <brennan@reverseproductions.com>
2012-04-13 20:00:45 -0700

    saving latest

--- layouts/wide.php
+++ layouts/wide.php
@@ -11,8 +11,16 @@
 <meta name="title" content="<?= site_title($sub_title, $page_title, $site_title) ?>" />
 <meta name="description" content="<?= $site_description ?>" />
 <meta name="keywords" content="<?= $site_keywords ?>" />
-<meta name="author" content="">
-<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="author" content="">             
+<?php if ($this->agent->is_mobile()): ?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no;" />
+<?php else: ?>
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+<?php endif; ?>
+
+<link rel="shortcut icon" href="<?= $site_assets ?>favicon.ico" />
+<link rel="icon" type="image/png" href="<?= $site_assets ?>emoome_32.png" />
+<link rel="apple-touch-icon-precomposed" href="<?= $site_assets ?>apple-touch-icon-precomposed.png" />  
 
 <?= $head ?>
 </head>
@@ -20,7 +28,6 @@
 <header>
 	<?= $logged ?>		
 </header>
-
 
 <div id="container">
 	<div id="content_message" class="message_normal"><?= $message ?></div>
