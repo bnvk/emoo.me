ccc615280fcf2d0f08a2efb31c42ae737449af08
Brennan Novak <brennan@reverseproductions.com>
2012-04-22 13:29:41 -0700

    saving latest

--- partials/logged.php
+++ partials/logged.php
@@ -9,9 +9,9 @@
 		<li><a class="button" href="<?= $link_logout ?>">Log Out</a></li>
 	<?php else: ?>
 		<?php if (config_item('users_signup') == 'TRUE'): ?>
-		<li><a class="button" href="<?= base_url().'signup' ?>">Sign Up</a></li>
+		<li><a class="button" id="button_signup" href="<?= base_url().'#!/signup' ?>">Sign Up</a></li>
 		<?php endif; ?>
-		<li><a class="button" href="<?= base_url().'login' ?>">Log In</a></li>
+		<li><a class="button" id="button_login" href="<?= base_url().'#!/login' ?>">Log In</a></li>
 	<?php endif; ?>
 	</ul>
 	<div class="clear"></div>
